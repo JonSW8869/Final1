117c23bb347c0fcd8e4437ff0419c6ffd629750c
JonSW8869 <143462653+JonSW8869@users.noreply.github.com>
2023-12-13 17:03:43 -0500

    l

---
+++ vite-project/src/routes/contact.jsx
@@ -0,0 +1,32 @@
+
+
+export default function Contact() {
+  const contact = {
+    first: "Your",
+    last: "Name",
+    twitter: "your_handle",
+    notes: "Some notes",
+    favorite: true,
+  };
+
+  return (
+    <div id="contact">
+    <artlicle className ="Strings">
+      <div>
+        <h1>Strings</h1>
+        <h2>Declaring a String</h2>
+            <header>Hello World!</header>
+            <body>
+               <p>Strings in C++ are declared like any other variable in Java: (variableType) Variable Name.</p>
+              <p>For example:</p>
+              <p>string Hello;</p>
+        <h2>String Methods</h2>
+            <p>There are a lot of String Methods</p>
+            <p>For example:</p>
+            <p>String.size()</p>
+        </body>
+      </div>
+      </artlicle>
+    </div>
+  );
+}
