fb517f19b93a4fa4744435369cb665fedea87102
JonSW8869 <143462653+JonSW8869@users.noreply.github.com>
2023-12-13 18:20:17 -0500

    v

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import * as ReactDOM from "react-dom/client";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import "./index.css";
-import Root from "./routes/root";
-import Contact from "./routes/contact";
-
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root />,
-    children: [
-        {
-          path: "contacts/:contactId",
-          element: <Contact />,
-        },
-      ],
+    element: 
+    <div>
+       <artlicle className ="Strings">
+      <div>
+        <h1>Strings</h1>
+        <h2>Declaring a String</h2>
+            <header>Hello World!</header>
+            <body>
+               <p>Strings in C++ are declared like any other variable in Java: (variableType) Variable Name.</p>
+              <p>For example:</p>
+              <p>string Hello;</p>
+        <h2>String Methods</h2>
+            <p>There are a lot of String Methods</p>
+            <p>For example:</p>
+            <p>String.size()</p>
+        </body>
+      </div>
+      </artlicle>
+    </div>,
   },
 ]);
 
