a458203bc2ab7003ef4345989d3ce8faf9a4d567
JonSW8869 <143462653+JonSW8869@users.noreply.github.com>
2023-12-17 10:10:48 -0500

    PUSHHHHH

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -22,7 +22,7 @@ const router = createBrowserRouter([
               <p>For example:</p>
              <center><p>string Hello = "Hello World!";</p></center>
         <h2>String Methods</h2>
-            <p>There are a lot of String Methods</p>
+            <p>There are a few key of String Methods</p>
             <p>For example:</p>
             <li>String.size()</li>
             <li>String.length()</li>
@@ -57,7 +57,7 @@ const router = createBrowserRouter([
             <body>
               <p>First you start with what kind of array you want.</p>
               <p>Typically they are either strings or intergers and are declared as such:</p>
-             <center><p>string arr[] = {"Hello World!"};</p></center>
+             <center><p>string arr[] = {'{"'}Hello World!{'"}'};</p></center>
         <h2>Pointers</h2>
             <p>Sometimes instead of strings and ints, arrays could store pointers</p>
             <p>For example:</p>
@@ -79,7 +79,7 @@ const router = createBrowserRouter([
             <p>Here is what makes vectors different from arrays</p>
             <p>Functions:</p>
             <li>vector.pushback()</li>
-            <li>String.popback()</li>
+            <li>vector.popback()</li>
             <li>All other array functions</li>
             <p>pushback adds the item at the last index of your vector.</p>
             <p>popback will destory the last index along with its item.</p>
@@ -92,10 +92,10 @@ const router = createBrowserRouter([
         <h1>Why use C++?</h1>
         <h2>Its popular</h2>
         <p>Many companies, schools, or any CS related business will ask you to use  one of the big three:</p>
-        <l1>Python</l1>
-        <l1>Java</l1>
-        <l1>C++</l1>
-        <p>Learning C++ can help you land more oppertunites at work.</p>
+        <li>Python</li>
+        <li>Java</li>
+        <li>C++</li>
+        <p>Learning C++ can help you land more oppertunites at work and it will help in learning other languages similar to C++.</p>
         <h2>It helps with your CS skils in general</h2>
         <p>Like Python and Java, C++ helps your understanding of CS in general. Through C++, you learn about how information is stored and used in programs. 
           What C++ differs from Java and Python is it's unique learning curve of memory allocation. In C++ you are introduced with pointers which directly mess with your computer's memory.
@@ -105,15 +105,15 @@ const router = createBrowserRouter([
     </artilcle>
 
     <h1>Conclusions</h1>
-    <p>Thank you so much for looking at this site. Although I'm pretty sure its just going to be you Professor Lynch who will look at it.
-      In that case, thank you so much for being my professor. I had a great time in your class and you were always so helpful. I hope you have a wonderful break. 
+    <p>Thank you so much for looking at this site. Although I'm pretty sure its just going to be you, Professor Lynch, who will look at it.
+      In that case, thank you so much for being my professor, I had a great time in your class and you were always so helpful. I hope you have a wonderful break. 
     </p>
 
     <p>
       If you are not my professor, then I have no clue how you got onto this site but thank you so much nonetheless. Hope you learned a little more about C++ and I hope you have a fantastic day! 
     </p>
 
-    <p><ps>PS: Don't look at the code for this website. For some reason things would not work at all and I didn't have the time to right my wrongs. I wanted to do a cool side bar on the side for the topics but for some reason it works on the wrong repo and not this one. I think my Node.JS is corrupted or the React stuff didn't copy over right? Anyways this code is the only way this website works so please don't question it. :D</ps></p>
+    <p><ps>PS: Don't look too deep at the code for this website. For some reason things would not work at all and I didn't have the time to right my wrongs. I wanted to do a cool side bar on the side for the topics but for some reason it works on a different repo and but not this one despite being the same code. I think my Node.JS is corrupted or my package.json was improperly imported. Anyways this code is the only way this website works so please don't question it. Thanks, :{')'}</ps></p>
     
     </div>,
   },
